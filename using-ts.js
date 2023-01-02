83c54e885e6d129d954daba1bbebba0a5e37f289
Alex <alexbelkacoder@gmail.com>
2023-01-02 15:26:01 +0300

    добавил features

--- using-ts.js
+++ using-ts.js
@@ -1,16 +1,22 @@
 var button = document.querySelector("button");
 var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
-function add(num1, num2) {
-  if (typeof num1 !== "number" || typeof num2 !== "number") {
-    throw new Error("Incorrect input!!!");
-  }
-  return num1 + num2;
+function add(num1, num2, showResult) {
+    if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+        throw new Error('Incorrect input!!!');
+    }
+    if (showResult) {
+        console.log('getSum:', num1 + num2);
+    }
+    else {
+        return num1 + num2;
+    }
 }
-var firstArg = "10";
+var firstArg = 10;
 var secondArg = 20;
-var result = add(firstArg, secondArg);
-console.log("result:", result);
+var printResult = true;
+var result = add(firstArg, secondArg, printResult);
+console.log('result:', result);
 button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
+    console.log(add(+input1.value, +input2.value));
 });
--- using-ts.ts
+++ using-ts.ts
@@ -2,17 +2,27 @@ const button = document.querySelector("button");
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1:number, num2:number) {
+
+
+function add(num1:number, num2:number,showResult:boolean) {
 if(typeof num1 !== 'number' || typeof num2 !== 'number'){
  throw new Error('Incorrect input!!!');
 }
+
+if(showResult){
+console.log('getSum:',num1 + num2)
+}
+else{
   return num1 + num2;
 }
+}
+
 
 const firstArg = 10;
 const secondArg = 20; 
+const printResult = true;
 
-const result = add(firstArg,secondArg);
+const result = add(firstArg,secondArg,printResult);
 console.log('result:',result);
 
 button.addEventListener("click", function() {
