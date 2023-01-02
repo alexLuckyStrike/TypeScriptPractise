fea22a4470c8294395800b411dd0de1fcbe640f8
Alex <alexbelkacoder@gmail.com>
2023-01-02 14:53:31 +0300

    add some features

--- using-ts.js
+++ using-ts.js
@@ -2,8 +2,15 @@ var button = document.querySelector("button");
 var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
 function add(num1, num2) {
-    return num1 + num2;
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    throw new Error("Incorrect input!!!");
+  }
+  return num1 + num2;
 }
+var firstArg = "10";
+var secondArg = 20;
+var result = add(firstArg, secondArg);
+console.log("result:", result);
 button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
+  console.log(add(+input1.value, +input2.value));
 });
--- using-ts.ts
+++ using-ts.ts
@@ -3,8 +3,17 @@ const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
 function add(num1:number, num2:number) {
+if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+ throw new Error('Incorrect input!!!');
+}
   return num1 + num2;
 }
+
+const firstArg = 10;
+const secondArg = 20; 
+
+const result = add(firstArg,secondArg);
+console.log('result:',result);
 
 button.addEventListener("click", function() {
   console.log(add(+input1.value, +input2.value));
